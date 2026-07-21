a3aa53c060b3be7d1f510a7b46e85c59070f647e
satheeshkumar-paramasivam17 <satheeshkumar.paramasivam17@gmail.com>
2026-07-21 13:59:35 +0530

    Test automatic Jenkins trigger

--- src/main/java/com/satheesh/jenkins_demo/controller/HelloController.java
+++ src/main/java/com/satheesh/jenkins_demo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Automatic Jenkins build is working!";
+        return "Automatic Jenkins polling is working!";
     }
 }
